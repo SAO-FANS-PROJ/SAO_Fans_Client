880aaa0e0fbd7a4165c57480d2570bb7a212a583
cfdxkk <b2567240058@gmail.com>
2022-06-03 20:26:14 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import app from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from "axios"
+import lax from 'lax.js'
 
-createApp(App).use(store).use(router).mount('#app')
+
+let App = createApp(app)
+App.config.globalProperties.Axios = axios
+App.config.globalProperties.LaxAnime = lax
+App.use(store)
+App.use(router)
+// App.use(ElementPlus)
+App.mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="selector">Hello</div>
+
+    <div style="height: 10000px; width: 20px; background-color: pink"></div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: "testAbout",
+  mounted() {
+
+    console.log('aaaaaaaaaaaaaaaaaaaa')
+    this.LaxAnime.init()
+
+    // Add a driver that we use to control our animations
+    this.LaxAnime.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    this.LaxAnime.addElements('.selector', {
+          scrollY: {      // Animation data
+            opacity: [
+              [0, 300],
+              [1, 0]
+            ],
+
+          },
+        },
+        {
+          style: {position: 'fixed', top: '200px', left: '200px'},   // Element options
+          modValue: 1200
+        })
+  }
+}
+
+  // window.onload = function () {
+  //   this.LaxAnime.init()
+  //
+  //   // Add a driver that we use to control our animations
+  //   this.LaxAnime.addDriver('scrollY', function () {
+  //     return window.scrollY
+  //   })
+  //
+  //   // Add animation bindings to elements
+  //   this.LaxAnime.addElements('.selector', {
+  //     scrollY: {
+  //       translateX: [
+  //         ["elInY", "elCenterY", "elOutY"],
+  //         [0, 'screenWidth/2', 'screenWidth'],
+  //       ]
+  //     }
+  //   })
+  // }
+
+
+</script>
