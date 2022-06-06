8f9b503711fe3dc54239f8926b607c71ac640135
cfdxkk <b2567240058@gmail.com>
2022-06-06 23:05:03 +0800

    Re: 做了一堆三角形 :D (修复bug/完善)
    Re: 做了一堆三角形 :D (修复bug)
    
    修复了添加三角形速度和删除三角形速度不一致的问题
    增加了一个守卫程序，用于出现错误时释放内存/CPU
    增加了新的动画样式
    优化原有动画

--- src/components/backGroundFloatTriangle/BackGroundFloatTriangle.vue
+++ src/components/backGroundFloatTriangle/BackGroundFloatTriangle.vue
@@ -9,7 +9,7 @@ export default {
   name: "BackGroundFloatTriangle",
   data: () => {
     return {
-      floatTriangles: []
+      // floatTriangles: []
     }
   },
   methods: {
@@ -27,7 +27,17 @@ export default {
       triangle.className = triangleBoxClassName
 
       // 计算当前屏幕比例相对于标准16:9的比例大多少或者小多少  get the offset to basic 16:9 screen
-      const screenRatio = pageWidth / pageHeight > 1.2 ? pageWidth / pageHeight : 1.2  // 如果屏幕比例大于 1.2 则取屏幕比例，如果小于 1.2 则取 1.2
+      const minRatio = 1.5
+      let screenRatio = minRatio
+      if (pageWidth / pageHeight > minRatio) {  // 如果屏幕比例大于 1.5 则
+        if (pageWidth / pageHeight > 2) {  // 判断是否大于2，如果大于2则取2
+          screenRatio = 2
+        } else {  // 如果小于2则取真是比例
+          screenRatio = pageWidth / pageHeight
+        }
+      } else {  // 否则如果小于 1.5 则取 1.5
+        screenRatio = minRatio
+      }
       const basicScreenRatio = 1.7777777777    // 16:9 is 1.7777777777
       const screenRatioOffset = screenRatio / basicScreenRatio
 
@@ -54,18 +64,16 @@ export default {
       const triangleBox = document.getElementById('float-triangle-box')
       if (triangleBox !== null && triangleBox !== undefined) {
         triangleBox.appendChild(triangle)
-        // const newFloatTriangles =
-        this.floatTriangles.push(triangleBoxId)
         return triangleBoxId
       } else {
         return null
       }
     },
     trianglesAnimate() {
-      const triangleBoxList = this.floatTriangles  // 获取三角形的盒子的元素ID列表
-      if (triangleBoxList !== undefined && triangleBoxList !== null && triangleBoxList.length !== 0) {  // 非空判断
+      const triangleBoxList = document.getElementsByClassName('float-triangle')  // 获取三角形的盒子的元素ID列表
+      if (triangleBoxList !== undefined && triangleBoxList !== null) {  // 非空判断
         for(let i = 0; i < triangleBoxList.length; i++) {  // 循环获取三角形的盒子的元素ID
-          const triangleBox = document.getElementById(triangleBoxList[i])  // 获取三角形的盒子的元素
+          const triangleBox = triangleBoxList[i]  // 获取三角形的盒子的元素
           this.triangleAnimate(triangleBox, true)  // 为三角形的盒子的元素添加三角形裁剪动画和位移动画
         }
       }
@@ -105,7 +113,7 @@ export default {
 
       // 三角形变形动画随机播放速度
       const minTrianglePointAnimeDuration = 18000
-      const maxTrianglePointAnimaDuration = 23000
+      const maxTrianglePointAnimaDuration = 30000
       const trianglePointAnimeDuration = Math.floor( Math.random() * (maxTrianglePointAnimaDuration - minTrianglePointAnimeDuration) + minTrianglePointAnimeDuration )
 
       // 三角形顶点的随机坐标（动画开始）
@@ -135,16 +143,31 @@ export default {
       const maxPoint3AnimeEndDegree = 90
       const point3AnimeEndDegree = Math.floor( Math.random() * (maxPoint3AnimeEndDegree - minPoint3AnimeEndDegree) + minPoint3AnimeEndDegree )
 
+      const chooseAnimeKeyFrame = Math.floor(Math.random() * 5)  // 生成 0-4的一个随机数
+      console.log('chooseAnimeKeyFrame', chooseAnimeKeyFrame)
+      const animeKeyFrameList = [  // 生成两种不同的动画
+        [
+          {
+            clipPath: `polygon(0 ${point1AnimeStartDegree}%, 100% ${point2AnimeStartDegree}%, 0 ${point3AnimeStartDegree}%)`
+          },
+          {
+            clipPath: `polygon(0 ${point1AnimeEndDegree}%, 100% ${point2AnimeEndDegree}%, 0 ${point3AnimeEndDegree}%)`
+          }
+        ],
+        [
+          {
+            clipPath: `polygon(0 ${point2AnimeStartDegree}%, 100% ${point1AnimeStartDegree}%, 0 ${point3AnimeStartDegree}%)`
+          },
+          {
+            clipPath: `polygon(0 ${point2AnimeEndDegree}%, 100% ${point1AnimeEndDegree}%, 0 ${point3AnimeEndDegree}%)`
+          }
+        ],
+      ]
+      const animeKeyFrame = chooseAnimeKeyFrame < 4 ?  animeKeyFrameList[0] : animeKeyFrameList[0]  // 有 4/5 的概率使用动画0 有 1/5 的概率使用动画1
+
       // 增加三角形尺寸变化的动画
       triangleBox.animate(
-          [
-            {
-              clipPath: `polygon(0 ${point1AnimeStartDegree}%, 100% ${point2AnimeStartDegree}%, 0 ${point3AnimeStartDegree}%)`
-            },
-            {
-              clipPath: `polygon(0 ${point1AnimeEndDegree}%, 100% ${point2AnimeEndDegree}%, 0 ${point3AnimeEndDegree}%)`
-            }
-          ],
+          animeKeyFrame,
           {
             duration: trianglePointAnimeDuration,
             easing: 'ease-in-out',
@@ -194,10 +217,11 @@ export default {
 
     },
     triangleDisappear(Duration) {
-      const triangleListLength = this.floatTriangles.length  // 获取三角形列表的长度
-      if (triangleListLength > 0){  // 如果大于零
+      const triangleListLength = document.getElementsByClassName('float-triangle').length  // 获取三角形的数量
+      const triangleListList = document.getElementsByClassName('float-triangle')  // 获取三角形的数量
+      if (triangleListLength > 6){  // 如果三角形的数量大于 6 才会执行清除
         const oneOfTriangleList = Math.floor(Math.random() * triangleListLength)  // 三角形列表中随机选一个
-        const disappearTriangle = document.getElementById(this.floatTriangles[oneOfTriangleList])  // 获取一个倒霉蛋
+        const disappearTriangle = triangleListList[oneOfTriangleList] // 获取一个倒霉蛋
         if (disappearTriangle !== undefined && disappearTriangle !== null) {  // 判断是否获取到 dom
           // 生成随机帧序列
           const grayFrameAndNothingnessFrame = [{backgroundColor: 'var(--float-triangle-color)'}, {backgroundColor: 'var(--nothingness)'}] // 正常帧和空白帧
@@ -221,21 +245,41 @@ export default {
               }
           )
 
-          const _this = this
           // 动画结束后将其从 dom 和 三角形列表 中删除
           setTimeout(() => {
             disappearTriangle.remove()
-            _this.floatTriangles.splice(oneOfTriangleList, 1)
           }, Duration)
 
         }
       }
     },
+    systemProtect() {  // 系统保护方法，用于在因为异常大量堆积三角形的时候释放系统占用，当三角形数量过少时增加数量
+      const _this = this
+      let triangleDomNumbers = document.getElementsByClassName('float-triangle').length // 获取三角形 dom 数量
+      if ( triangleDomNumbers < 3) {  // 如果三角形总数小于 3
+        for (let i = 0; i < 10; i++) {  // 循环10次
+            const newTriangleBoxId = _this.createTriangleBox(triangleDomNumbers++)
+            const newTriangleBox = document.getElementById(newTriangleBoxId)
+            _this.triangleAnimate(newTriangleBox, false)
+        }
+      } else if (triangleDomNumbers > 20) {  // 如果三角形数量大于30
+        for (let i = 0; i < triangleDomNumbers; i++) {  // 循环 "三角形数量" 遍
+            this.triangleDisappear(500)  // 使用 500ms 的清除动画
+        }
+      } else if (triangleDomNumbers > 50) {  // 如果三角形数量大于50
+        document.getElementById('float-triangle-box').innerHTML = ''   //直接移除 dom
+      }
+    },
     initTriangle() {
 
       const _this = this
-      let boxCount = 12
 
+      // 获取屏幕尺寸 get page size
+      const pageHeight = document.body.clientHeight ?? document.documentElement.clientHeight
+      const pageWidth = document.body.clientWidth ?? document.documentElement.clientWidth
+
+      // 生成的三角形数量
+      let boxCount = pageHeight > pageWidth ? 9 : 12
       // 生成12个三角形的盒子
       for (let i = 0; i < boxCount; i++) {
         _this.createTriangleBox(i)
@@ -243,33 +287,51 @@ export default {
       // 给三角形应用动画
       _this.trianglesAnimate()
 
-      // 移除或添加三角形的随机时间
+      // 移除或添加三角形的随机间隔
       const AppearAndDisappearSpeedMin = 22000
       const AppearAndDisappearSpeedMax = 38000
       const AppearAndDisappearSpeed = Math.round( Math.random() * (AppearAndDisappearSpeedMax - AppearAndDisappearSpeedMin) + AppearAndDisappearSpeedMin )
-      const AppearAndAppearSpeed = Math.round( Math.random() * (AppearAndDisappearSpeedMax - AppearAndDisappearSpeedMin) + AppearAndDisappearSpeedMin )
 
       // 闪烁动画持续时间
-      const minTriangleDisappearAnimeDuration = 400
-      const maxTriangleDisappearAnimaDuration = 700
+      const minTriangleDisappearAnimeDuration = 500
+      const maxTriangleDisappearAnimaDuration = 750
       const triangleDisappearAnimeDuration = Math.floor(Math.random() * (maxTriangleDisappearAnimaDuration - minTriangleDisappearAnimeDuration) + minTriangleDisappearAnimeDuration)
 
-      // 移除一个元素
+      // 移除三角形和添加三角形的时间差
+      const minTriangleDisappearAnimeWaitTIme = 2000
+      const maxTriangleDisappearAnimaWaitTIme = 3000
+      const triangleDisappearAnimeWaitTIme = Math.floor(Math.random() * (maxTriangleDisappearAnimaWaitTIme - minTriangleDisappearAnimeWaitTIme) + minTriangleDisappearAnimeWaitTIme)
+
+
+      // 移除一个三角形
       setInterval(() => {
-        _this.triangleDisappear(triangleDisappearAnimeDuration)
+        _this.triangleDisappear(triangleDisappearAnimeDuration)  // 移除一个三角形
       }, AppearAndDisappearSpeed)
 
-      // 生成一个元素
-      setInterval(() => {
-        const newTriangleBoxId = _this.createTriangleBox(boxCount++)
-        const newTriangleBox = document.getElementById(newTriangleBoxId)
-        _this.triangleAnimate(newTriangleBox, false)
-      }, AppearAndAppearSpeed + triangleDisappearAnimeDuration)
+      setTimeout( () => {  // 设置消失动画和添加动画之间的时间差
+
+        // 生成一个元素
+        setInterval(() => {
+          // 生成一个三角形
+          const newTriangleBoxId = _this.createTriangleBox(boxCount++)
+          const newTriangleBox = document.getElementById(newTriangleBoxId)
+          _this.triangleAnimate(newTriangleBox, false)
+        }, AppearAndDisappearSpeed)
+
+      }, triangleDisappearAnimeWaitTIme)
+
+
+      // 启动系统保护程序
+      setInterval( () => {
+        this.systemProtect()
+      }, 10000)
 
     },
+
   },
   mounted() {
     this.initTriangle()
+
   }
 }
 </script>
